d1933c495369ebf8929efa66cd090b7345d1d8ca
ZOOY2ON <112409907+ZOOY2ON@users.noreply.github.com>
2022-09-19 16:06:52 +0900

    [2DGP] 0914 강의 실습 업로드

---
+++ Drill 04/draw_circle_while.py
@@ -0,0 +1,12 @@
+import turtle
+
+count = 10
+
+while (count > 0):
+          turtle.forward(100)
+          turtle.left(30)
+          count = count - 1
+
+
+//count가 0보다 크면 계속해서 반복한다. 뭘?
+//(turtle을 앞으로 100이동, 그리고 왼쪽으로 30도 회전, 그리고 count 값 하나 감소)
---
+++ Drill 04/drunken_turtle.py
@@ -0,0 +1,9 @@
+import turtle
+import random
+
+turtle.shape('turtle')
+
+while (True):
+          turtle.setheading(random.randint(0,50))
+          turtle.forward(random.randint(50,120))
+          turtle.stamp()
